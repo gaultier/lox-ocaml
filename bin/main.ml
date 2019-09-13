f13d4e8ee984e64fb36bab76e62f1b979d0c0668
Philippe Gaultier <philippe.gaultier@ppro.com>
2019-09-13 08:03:37 +0200

    fix order

--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,4 @@
 ;;
 let tokens = Lox.Lex.lex "(){},.-+;*/@" in
-List.iter (fun t -> Printf.printf "%s\n" (Lox.Lex.lex_token_to_s t)) tokens
+tokens |> List.rev
+|> List.iter (fun t -> Printf.printf "%s\n" (Lox.Lex.lex_token_to_s t))
