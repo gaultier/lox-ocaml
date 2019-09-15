6db7051eccedf2f47cd28d7a9576ba5f5879a83d
Philippe Gaultier <philippe.gaultier@ppro.com>
2019-09-15 11:40:58 +0200

    Correctly handle closing string quote

--- lib/lex.ml
+++ lib/lex.ml
@@ -19,7 +19,7 @@ type lex_token =
   | Greater
   | GreaterEqual
   | LexString of char list
-  | Unknown of char
+  | Unknown of char list
 
 let rec lex_r acc irest =
   match irest with
@@ -73,11 +73,15 @@ let rec lex_r acc irest =
       lex_r acc irest
   | '\r' :: irest ->
       lex_r acc irest
-  | '"' :: irest ->
+  | '"' :: irest -> (
       let s, r = Base.List.split_while irest ~f:(fun c -> c != '"') in
-      lex_r (LexString s :: acc) r
+      match r with
+      | '"' :: rrest ->
+          lex_r (LexString s :: acc) rrest
+      | _ ->
+          lex_r (Unknown s :: acc) r )
   | x :: irest ->
-      lex_r (Unknown x :: acc) irest
+      lex_r (Unknown [x] :: acc) irest
 
 let lex_token_to_s c =
   match c with
@@ -122,6 +126,6 @@ let lex_token_to_s c =
   | LexString s ->
       "LexString=`" ^ Base.String.of_char_list s ^ "`"
   | Unknown c ->
-      "Unknown=`" ^ String.make 1 c ^ "`"
+      "Unknown=`" ^ Base.String.of_char_list c ^ "`"
 
 let lex s = lex_r [] (Base.String.to_list s)
