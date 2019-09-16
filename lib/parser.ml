f08e76a5d3a2bd0a96df2173ae90d7bc91d0b99e
Philippe Gaultier <philippe.gaultier@ppro.com>
2019-09-16 14:08:04 +0200

    fix modules

--- lib/parser.ml
+++ lib/parser.ml
@@ -1,1 +1,7 @@
-type expr = Binary | Grouping | Literal | Unary
+type expr =
+  | Binary of expr * Lex.lex_token * expr
+  | Grouping of expr
+  | Literal of float
+  | Unary of Lex.lex_token * expr
+
+let e = Binary (Unary (Minus, Literal 1.), Plus, Unary (Plus, Literal 2.))
