9009be0d469ceb65dc78c787b3c8c21ecbac9a48
Philippe Gaultier <philippe.gaultier@ppro.com>
2019-09-18 12:09:51 +0200

    add test for mult

--- lib/parser.ml
+++ lib/parser.ml
@@ -123,3 +123,6 @@ let%test _ =
 let%test _ = primary [Lex.LexNumber 1.] = (Literal (EFloat 1.), [])
 
 let%test _ = multiplication [Lex.LexNumber 1.] = (Literal (EFloat 1.), [])
+
+let%test _ = multiplication [Lex.LexNumber 1.; Lex.Star; Lex.LexNumber 2.] = (Binary (Literal (EFloat 1.), Lex.Star, Literal (EFloat 2.)), [])
+
