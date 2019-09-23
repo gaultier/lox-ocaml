c0dbaac83e44616ba7a3e890e15c89f6fe9556b5
Philippe Gaultier <philippe.gaultier@ppro.com>
2019-09-23 17:21:23 +0200

    rename

--- bin/main.ml
+++ bin/main.ml
@@ -5,9 +5,9 @@ in
 tokens |> List.iter (fun t -> Printf.printf "%s\n" (Lox.Lex.lex_token_to_s t))
 
 ;;
-"-1 + 2 / -3 >= 5 == 6" |> Lox.Lex.lex |> Lox.Parser.expression |> fst
-|> Lox.Parser.expr_to_s |> print_endline
+"-1 + 2 / -3 >= 5 == 6" |> Lox.Lex.lex |> Lox.Parse.expression |> fst
+|> Lox.Parse.expr_to_s |> print_endline
 
 ;;
-"(1 != 3) == -2" |> Lox.Lex.lex |> Lox.Parser.expression |> fst
-|> Lox.Parser.expr_to_s |> print_endline
+"(1 != 3) == -2" |> Lox.Lex.lex |> Lox.Parse.expression |> fst
+|> Lox.Parse.expr_to_s |> print_endline
