6dc879c75c3174beebc70ccff38ff10220b8242d
Philippe Gaultier <philippe.gaultier@ppro.com>
2019-09-26 08:56:10 +0200

    Print sepx

--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,12 @@
 ;;
+"\"hey\" == \"hey\"" |> Lox.Lex.lex
+|> List.iter (fun t ->
+       Lox.Lex.sexp_of_lex_token t
+       |> Base.Sexp.to_string |> Printf.printf "%s ")
+
+;;
+print_endline ""
+
+;;
 "\"hey\" == \"hey\"" |> Lox.Lex.lex |> Lox.Parse.expression |> fst
 |> Lox.Interpret.eval |> Lox.Parse.literal_to_s |> print_endline
