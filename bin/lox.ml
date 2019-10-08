ca0d12e219006e42db9310c3152218381cba86d0
Philippe Gaultier <philippe.gaultier@ppro.com>
2019-10-08 16:30:42 +0200

    --wip-- [skip ci]

--- bin/lox.ml
+++ bin/lox.ml
@@ -5,18 +5,14 @@ let buf = Buffer.create 16 in
   let try_read () =
     try Some (input_line ic) with End_of_file -> None in
   let rec loop () = match try_read () with
-    | Some s ->  Buffer.add_string buf s;  loop ()
-    | None -> close_in ic; 
-  loop ()
+    | Some s ->  Buffer.add_string buf s;  loop ();
+    | None -> close_in ic ;
+in
+  loop (); buf
 
-let loxc env =
-  
-  let stmts, env =
-  read_lines "./hello_world.lox" |> Base.Buffer.To_string (String)  |> Lox.Lex.lex |> Lox.Parse.parse
-    |> Lox.Interpret.interpret env
-  in
-  Array.iter Lox.Interpret.print stmts ;
-  repl env
+let loxc () =
+  read_lines "./hello_world.lox" |> Base.Buffer.contents |> Lox.Lex.lex |> Lox.Parse.parse
+    |> Lox.Interpret.interpret Lox.Interpret.StringMap.empty
 
 let rec repl env =
   Printf.printf "> " ;
