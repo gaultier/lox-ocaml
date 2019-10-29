5b1608c7c027c9975baa347247be6a30e3075057
Philippe Gaultier <philippe.gaultier@ppro.com>
2019-10-29 10:03:30 +0100

    remove env from FunctionReturn

--- lib/interpret.ml
+++ lib/interpret.ml
@@ -2,7 +2,7 @@ open Parse
 
 let empty = Base.Map.empty (module Base.String)
 
-exception FunctionReturn of value * environment
+exception FunctionReturn of value
 
 let rec find_in_environment env v =
   match Base.Map.find env.values v with
@@ -171,8 +171,8 @@ let rec eval s env =
       in
       (Nil, env)
   | Return (_, expr) ->
-      let v, env = eval_exp expr env in
-      raise (FunctionReturn (v, env))
+      let v, _ = eval_exp expr env in
+      raise (FunctionReturn v)
   | Function ({Lex.kind= Lex.Identifier name; _}, decl_args, body) ->
       let fn =
         Callable
@@ -199,7 +199,7 @@ let rec eval s env =
                     body
                   |> ignore ;
                   (Nil, env)
-                with FunctionReturn (v, _) -> (v, env)) }
+                with FunctionReturn v -> (v, env)) }
       in
       env.values <- Base.Map.set ~key:name ~data:fn env.values ;
       (Nil, env)
