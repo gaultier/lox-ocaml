99d965cec59ca10ec0780d962934ab7e6e3a4636
Philippe Gaultier <philippe.gaultier@ppro.com>
2019-11-05 07:30:07 +0100

    open lex in parser

--- lib/parse.ml
+++ lib/parse.ml
@@ -1,3 +1,4 @@
+open Lex
 open Base
 
 let ( let* ) x f = Result.bind x ~f
@@ -38,31 +39,31 @@ let globals =
           ; fn= (fun _ env -> (Number (Unix.gettimeofday ()), env)) } ) ]
 
 type expr =
-  | Binary of expr * Lex.token_kind * expr
+  | Binary of expr * token_kind * expr
   | Grouping of expr
   | Literal of value
-  | Unary of Lex.token_kind * expr
-  | Assign of Lex.token_kind * expr
-  | Variable of Lex.token_kind
+  | Unary of token_kind * expr
+  | Assign of token_kind * expr
+  | Variable of token_kind
   | LogicalOr of expr * expr
   | LogicalAnd of expr * expr
-  | Call of expr * Lex.token * expr list
+  | Call of expr * token * expr list
 
 type statement =
   | Expr of expr
   | Print of expr
-  | Var of Lex.token_kind * expr
+  | Var of token_kind * expr
   | Block of statement array
-  | Function of Lex.token * Lex.token list * statement list
-  | Return of Lex.token * expr
+  | Function of token * token list * statement list
+  | Return of token * expr
   | IfStmt of expr * statement
   | IfElseStmt of expr * statement * statement
   | WhileStmt of expr * statement
 
 let rec sync acc = function
-  (* | Lex.For :: _ as r -> *)
+  (* | For :: _ as r -> *)
   (*     (acc, r) *)
-  | ({Lex.kind= Lex.SemiColon; _} as t) :: (_ as r) ->
+  | ({kind= SemiColon; _} as t) :: (_ as r) ->
       (t :: acc, r)
   | [] ->
       (acc, [])
@@ -72,7 +73,7 @@ let rec sync acc = function
 let error ctx expected rest =
   let lines, columns =
     List.hd rest
-    |> Option.map ~f:(fun {Lex.lines; Lex.columns; _} -> (lines, columns))
+    |> Option.map ~f:(fun {lines; columns; _} -> (lines, columns))
     (*FIXME*)
     |> Option.value ~default:(1, 1)
   in
@@ -83,7 +84,7 @@ let error ctx expected rest =
         "no more tokens"
     | _ :: _ ->
         invalid
-        |> List.rev_map ~f:(fun {Lex.kind= x; _} -> Lex.token_to_string x)
+        |> List.rev_map ~f:(fun {kind= x; _} -> token_to_string x)
         |> List.fold ~init:"" ~f:(fun acc x -> acc ^ " " ^ x)
   in
   Result.fail
@@ -92,24 +93,24 @@ let error ctx expected rest =
     , rest )
 
 let rec primary = function
-  | {Lex.kind= Lex.False; _} :: rest ->
+  | {kind= False; _} :: rest ->
       Ok (Literal (Bool false), rest)
-  | {Lex.kind= Lex.True; _} :: rest ->
+  | {kind= True; _} :: rest ->
       Ok (Literal (Bool true), rest)
-  | {Lex.kind= Lex.Nil; _} :: rest ->
+  | {kind= Nil; _} :: rest ->
       Ok (Literal Nil, rest)
-  | {Lex.kind= Lex.Number f; _} :: rest ->
+  | {kind= Number f; _} :: rest ->
       Ok (Literal (Number f), rest)
-  | {Lex.kind= Lex.String s; _} :: rest ->
+  | {kind= String s; _} :: rest ->
       Ok (Literal (String s), rest)
-  | {Lex.kind= Lex.ParenLeft; _} :: rest -> (
+  | {kind= ParenLeft; _} :: rest -> (
       let* e, rest = expression rest in
       match rest with
-      | {Lex.kind= Lex.ParenRight; _} :: rest ->
+      | {kind= ParenRight; _} :: rest ->
           Ok (Grouping e, rest)
       | _ as rest ->
           error "Primary" "Expected closing parenthesis `)`" rest )
-  | {Lex.kind= Lex.Identifier _ as i; _} :: rest ->
+  | {kind= Identifier _ as i; _} :: rest ->
       Ok (Variable i, rest)
   | _ as rest ->
       error "Primary" "Expected primary (e.g `1` or `(true)` or \"hello\")"
@@ -118,26 +119,26 @@ let rec primary = function
 and fn_call tokens =
   let* expr, rest = primary tokens in
   match rest with
-  | {Lex.kind= Lex.ParenLeft; _} :: rest ->
+  | {kind= ParenLeft; _} :: rest ->
       fn_call_arguments expr [] rest
   | _ ->
       Ok (expr, rest)
 
 and fn_call_comma_argument = function
-  | {Lex.kind= Lex.Comma; _} :: rest ->
+  | {kind= Comma; _} :: rest ->
       expression rest
   | _ as rest ->
       error "Function call arguments" "Expected `,` before argument" rest
 
 and fn_call_comma_arguments args = function
-  | ({Lex.kind= Lex.ParenRight; _} as t) :: rest ->
+  | ({kind= ParenRight; _} as t) :: rest ->
       Ok (args, t, rest)
   | _ as rest ->
       let* arg, rest = fn_call_comma_argument rest in
       (fn_call_comma_arguments [@tailcall]) (arg :: args) rest
 
 and fn_call_arguments callee args = function
-  | ({Lex.kind= Lex.ParenRight; _} as t) :: rest ->
+  | ({kind= ParenRight; _} as t) :: rest ->
       Ok (Call (callee, t, args), rest)
   | _ as rest ->
       let* expr, rest = expression rest in
@@ -150,8 +151,7 @@ and fn_call_arguments callee args = function
       Ok (Call (callee, closing_paren, List.rev args), rest)
 
 and unary = function
-  | {Lex.kind= Lex.Bang as t; _} :: rest
-  | {Lex.kind= Lex.Minus as t; _} :: rest ->
+  | {kind= Bang as t; _} :: rest | {kind= Minus as t; _} :: rest ->
       let+ right, rest = unary rest in
       (Unary (t, right), rest)
   | _ as t ->
@@ -160,8 +160,7 @@ and unary = function
 and multiplication tokens =
   let* left, rest = unary tokens in
   match rest with
-  | {Lex.kind= Lex.Star as t; _} :: rest
-  | {Lex.kind= Lex.Slash as t; _} :: rest ->
+  | {kind= Star as t; _} :: rest | {kind= Slash as t; _} :: rest ->
       let+ right, rest = multiplication rest in
       (Binary (left, t, right), rest)
   | _ ->
@@ -170,8 +169,7 @@ and multiplication tokens =
 and addition tokens =
   let* left, rest = multiplication tokens in
   match rest with
-  | {Lex.kind= Lex.Plus as t; _} :: rest
-  | {Lex.kind= Lex.Minus as t; _} :: rest ->
+  | {kind= Plus as t; _} :: rest | {kind= Minus as t; _} :: rest ->
       let+ right, rest = addition rest in
       (Binary (left, t, right), rest)
   | _ ->
@@ -180,10 +178,10 @@ and addition tokens =
 and comparison tokens =
   let* left, rest = addition tokens in
   match rest with
-  | {Lex.kind= Lex.Greater as t; _} :: rest
-  | {Lex.kind= Lex.GreaterEqual as t; _} :: rest
-  | {Lex.kind= Lex.Less as t; _} :: rest
-  | {Lex.kind= Lex.LessEqual as t; _} :: rest ->
+  | {kind= Greater as t; _} :: rest
+  | {kind= GreaterEqual as t; _} :: rest
+  | {kind= Less as t; _} :: rest
+  | {kind= LessEqual as t; _} :: rest ->
       let+ right, rest = comparison rest in
       (Binary (left, t, right), rest)
   | _ ->
@@ -192,8 +190,7 @@ and comparison tokens =
 and equality tokens =
   let* left, rest = comparison tokens in
   match rest with
-  | {Lex.kind= Lex.BangEqual as t; _} :: rest
-  | {Lex.kind= Lex.EqualEqual as t; _} :: rest ->
+  | {kind= BangEqual as t; _} :: rest | {kind= EqualEqual as t; _} :: rest ->
       let+ right, rest = equality rest in
       (Binary (left, t, right), rest)
   | _ ->
@@ -207,7 +204,7 @@ and assignment = function
   | _ as t -> (
       let* e, rest = logic_or t in
       match rest with
-      | {Lex.kind= Lex.Equal; _} :: rest -> (
+      | {kind= Equal; _} :: rest -> (
           let* a, rest = assignment rest in
           match e with
           | Variable v ->
@@ -220,7 +217,7 @@ and assignment = function
 and logic_and tokens =
   let* l, rest = equality tokens in
   match rest with
-  | {Lex.kind= Lex.And; _} :: rest ->
+  | {kind= And; _} :: rest ->
       let+ r, rest = logic_and rest in
       (LogicalAnd (l, r), rest)
   | [] ->
@@ -231,7 +228,7 @@ and logic_and tokens =
 and logic_or tokens =
   let* l, rest = logic_and tokens in
   match rest with
-  | {Lex.kind= Lex.Or; _} :: rest ->
+  | {kind= Or; _} :: rest ->
       let+ r, rest = logic_or rest in
       (LogicalOr (l, r), rest)
   | [] ->
@@ -246,26 +243,26 @@ and expression_stmt = function
   | _ as t -> (
       let* stmt, rest = expression t in
       match rest with
-      | {Lex.kind= Lex.SemiColon; _} :: rest ->
+      | {kind= SemiColon; _} :: rest ->
           Ok (stmt, rest)
       | _ as rest ->
           error "Expression statement" "Expected closing semicolon `;`" rest )
 
 and print_stmt = function
-  | {Lex.kind= Lex.Print; _} :: rest ->
+  | {kind= Print; _} :: rest ->
       let+ expr, rest = expression_stmt rest in
       (Print expr, rest)
   | _ as rest ->
       error "Print statement" "Expected print statement (e.g `print 1;`)" rest
 
 and if_stmt = function
-  | {Lex.kind= Lex.If; _} :: {Lex.kind= Lex.ParenLeft; _} :: rest -> (
+  | {kind= If; _} :: {kind= ParenLeft; _} :: rest -> (
       let* e, rest = expression rest in
       match rest with
-      | {Lex.kind= Lex.ParenRight; _} :: rest -> (
+      | {kind= ParenRight; _} :: rest -> (
           let* then_stmt, rest = statement rest in
           match rest with
-          | {Lex.kind= Lex.Else; _} :: rest ->
+          | {kind= Else; _} :: rest ->
               let+ else_stmt, rest = statement rest in
               (IfElseStmt (e, then_stmt, else_stmt), rest)
           | _ ->
@@ -277,10 +274,10 @@ and if_stmt = function
         rest
 
 and while_stmt = function
-  | {Lex.kind= Lex.While; _} :: {Lex.kind= Lex.ParenLeft; _} :: rest -> (
+  | {kind= While; _} :: {kind= ParenLeft; _} :: rest -> (
       let* e, rest = expression rest in
       match rest with
-      | {Lex.kind= Lex.ParenRight; _} :: rest ->
+      | {kind= ParenRight; _} :: rest ->
           let+ s, rest = statement rest in
           (WhileStmt (e, s), rest)
       | _ ->
@@ -296,25 +293,25 @@ and for_stmt = function
   (* for (i;;i=i+1) *)
   (* for (;;i=i+1)  *)
   (* for (var i = 0; i < 5; i = i + 1) *)
-  | {Lex.kind= Lex.For; _} :: {Lex.kind= Lex.ParenLeft; _} :: rest ->
+  | {kind= For; _} :: {kind= ParenLeft; _} :: rest ->
       let* init_clause, rest =
         match rest with
-        | {Lex.kind= Lex.Var; _} :: _ ->
+        | {kind= Var; _} :: _ ->
             var_decl rest
-        | {Lex.kind= Lex.SemiColon; _} :: rest ->
+        | {kind= SemiColon; _} :: rest ->
             Ok (Expr (Literal (Bool true)), rest)
         | _ ->
             expression_stmt rest >>| fun (e, r) -> (Expr e, r)
       in
       let* stop_cond, rest =
         match rest with
-        | {Lex.kind= Lex.SemiColon; _} :: rest ->
+        | {kind= SemiColon; _} :: rest ->
             Ok (Literal (Bool true), rest)
         | _ -> (
             expression rest
             >>= fun (e, r) ->
             match r with
-            | {Lex.kind= Lex.SemiColon; _} :: r ->
+            | {kind= SemiColon; _} :: r ->
                 Ok (e, r)
             | _ ->
                 error "For-loop"
@@ -322,14 +319,14 @@ and for_stmt = function
       in
       let* incr_stmt, rest =
         match rest with
-        | {Lex.kind= Lex.ParenRight; _} :: _ ->
+        | {kind= ParenRight; _} :: _ ->
             Ok (Literal (Bool true), rest)
         | _ ->
             expression rest
       in
       let* _, rest =
         match rest with
-        | {Lex.kind= Lex.ParenRight; _} :: rest ->
+        | {kind= ParenRight; _} :: rest ->
             Ok (Nil, rest)
         | _ ->
             error "For-loop"
@@ -351,7 +348,7 @@ and block_stmt_inner tokens acc =
   match tokens with
   | [] ->
       error "Block statement" "Expected closing `}`" tokens
-  | {Lex.kind= Lex.CurlyBraceRight; _} :: rest ->
+  | {kind= CurlyBraceRight; _} :: rest ->
       Ok (acc, rest)
   | _ ->
       let* s, rest = declaration tokens in
@@ -359,21 +356,20 @@ and block_stmt_inner tokens acc =
       block_stmt_inner rest acc
 
 and block_stmt = function
-  | {Lex.kind= Lex.CurlyBraceLeft; _} :: rest ->
+  | {kind= CurlyBraceLeft; _} :: rest ->
       let+ stmts, rest = block_stmt_inner rest [||] in
       (Block stmts, rest)
   | _ as rest ->
       error "Block statement" "Expected block statement with opening `{`" rest
 
 and return_stmt = function
-  | ({Lex.kind= Lex.Return; _} as ret) :: {Lex.kind= Lex.SemiColon; _} :: rest
-    ->
+  | ({kind= Return; _} as ret) :: {kind= SemiColon; _} :: rest ->
       Ok (Return (ret, Literal Nil), rest)
-  | ({Lex.kind= Lex.Return; _} as ret) :: rest ->
+  | ({kind= Return; _} as ret) :: rest ->
       let* e, rest = expression rest in
       let* rest =
         match rest with
-        | {Lex.kind= Lex.SemiColon; _} :: rest ->
+        | {kind= SemiColon; _} :: rest ->
             Ok rest
         | _ as rest ->
             error "Return statement" "Expected terminating semicolon `;`" rest
@@ -386,43 +382,40 @@ and return_stmt = function
 and statement = function
   | [] as rest ->
       error "Statement" "Expected statement (e.g `x = 1;`)" rest
-  | {Lex.kind= Lex.Print; _} :: _ as t ->
+  | {kind= Print; _} :: _ as t ->
       print_stmt t
-  | {Lex.kind= Lex.CurlyBraceLeft; _} :: _ as t ->
+  | {kind= CurlyBraceLeft; _} :: _ as t ->
       block_stmt t
-  | {Lex.kind= Lex.If; _} :: _ as t ->
+  | {kind= If; _} :: _ as t ->
       if_stmt t
-  | {Lex.kind= Lex.While; _} :: _ as t ->
+  | {kind= While; _} :: _ as t ->
       while_stmt t
-  | {Lex.kind= Lex.For; _} :: _ as t ->
+  | {kind= For; _} :: _ as t ->
       for_stmt t
-  | {Lex.kind= Lex.Return; _} :: _ as t ->
+  | {kind= Return; _} :: _ as t ->
       return_stmt t
   | _ as t ->
       let+ e, rest = expression_stmt t in
       (Expr e, rest)
 
 and var_decl = function
-  | {Lex.kind= Lex.Var; _}
-    :: {Lex.kind= Lex.Identifier n; _} :: {Lex.kind= Lex.Equal; _} :: rest -> (
+  | {kind= Var; _} :: {kind= Identifier n; _} :: {kind= Equal; _} :: rest -> (
       let* e, rest = expression rest in
       match rest with
-      | {Lex.kind= Lex.SemiColon; _} :: rest ->
-          Ok (Var (Lex.Identifier n, e), rest)
+      | {kind= SemiColon; _} :: rest ->
+          Ok (Var (Identifier n, e), rest)
       | _ ->
           error "Variable declaration" "Expected terminating `;`" rest )
-  | {Lex.kind= Lex.Var; _}
-    :: {Lex.kind= Lex.Identifier n; _} :: {Lex.kind= Lex.SemiColon; _} :: rest
+  | {kind= Var; _} :: {kind= Identifier n; _} :: {kind= SemiColon; _} :: rest
     ->
-      Ok (Var (Lex.Identifier n, Literal Nil), rest)
+      Ok (Var (Identifier n, Literal Nil), rest)
   | _ as rest ->
       error "Variable declaration"
         "Expected variable declaration (e.g `var x = 1;`)" rest
 
 and function_decl = function
-  | {Lex.kind= Lex.Fun; _}
-    :: ({Lex.kind= Lex.Identifier _; _} as name)
-       :: {Lex.kind= Lex.ParenLeft; _} :: rest -> (
+  | {kind= Fun; _}
+    :: ({kind= Identifier _; _} as name) :: {kind= ParenLeft; _} :: rest -> (
       let* args, rest = fn_decl_arguments [] rest in
       let* block, rest = block_stmt rest in
       match block with
@@ -436,26 +429,25 @@ and function_decl = function
         "Expected function declaration (e.g `fun foo {print 1;}`)" rest
 
 and fn_decl_comma_argument = function
-  | {Lex.kind= Lex.Comma; _}
-    :: ({Lex.kind= Lex.Identifier _; _} as identifier) :: rest ->
+  | {kind= Comma; _} :: ({kind= Identifier _; _} as identifier) :: rest ->
       Ok (identifier, rest)
-  | {Lex.kind= Lex.Comma; _} :: {Lex.kind= Lex.ParenRight; _} :: _ as rest ->
+  | {kind= Comma; _} :: {kind= ParenRight; _} :: _ as rest ->
       error "Function declaration"
         "Trailing commas are not allowed in function arguments" rest
   | _ as rest ->
       error "Function declaration" "Expected `,` before argument" rest
 
 and fn_decl_comma_arguments args = function
-  | {Lex.kind= Lex.ParenRight; _} :: rest ->
+  | {kind= ParenRight; _} :: rest ->
       Ok (args, rest)
   | _ as rest ->
       let* arg, rest = fn_decl_comma_argument rest in
       (fn_decl_comma_arguments [@tailcall]) (arg :: args) rest
 
 and fn_decl_arguments args = function
-  | {Lex.kind= Lex.ParenRight; _} :: rest ->
+  | {kind= ParenRight; _} :: rest ->
       Ok (List.rev args, rest)
-  | ({Lex.kind= Lex.Identifier _; _} as identifier) :: rest ->
+  | ({kind= Identifier _; _} as identifier) :: rest ->
       let* args, rest = fn_decl_comma_arguments [identifier] rest in
       let len = List.length args in
       if len >= 255 then
@@ -469,9 +461,9 @@ and fn_decl_arguments args = function
 
 and declaration d =
   match d with
-  | {Lex.kind= Lex.Var; _} :: _ ->
+  | {kind= Var; _} :: _ ->
       (var_decl [@tailcall]) d
-  | {Lex.kind= Lex.Fun; _} :: _ ->
+  | {kind= Fun; _} :: _ ->
       (function_decl [@tailcall]) d
   | _ ->
       (statement [@tailcall]) d
