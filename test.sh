f2087595cfc8defcc5b1fa381ed86f0da4c7d01f
Philippe Gaultier <philippe.gaultier@ppro.com>
2019-11-06 12:38:31 +0100

    better test output

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -ex
+set -e
 
 if command -v patdiff
 then
@@ -10,7 +10,7 @@ fi
 
 for i in test/*.lox
 do
-    printf "Testing [%s]" "$i"
+    printf "Testing [%s]\n" "$i"
     ./lox run "$i" > "${i%.lox}.output" 2>&1
     $DIFFTOOL "${i%.lox}.expected" "${i%.lox}.output"
 done
