9ced2391a0d391c65a31af5794b70794c2eeb692
Philippe Gaultier <philippe.gaultier@ppro.com>
2019-11-06 12:52:14 +0100

    better test output

--- test.sh
+++ test.sh
@@ -10,7 +10,8 @@ fi
 
 for i in test/*.lox
 do
-    printf "Testing [%s]\n" "$i"
+    printf "%s" "$i"
     ./lox run "$i" > "${i%.lox}.output" 2>&1
     $DIFFTOOL "${i%.lox}.expected" "${i%.lox}.output"
+    printf " ✓\n"
 done
