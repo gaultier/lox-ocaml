d7b4511ff957c350f3cd888a7033546ebffa5b16
Philippe Gaultier <philippe.gaultier@ppro.com>
2019-11-10 10:05:42 +0100

    --wip-- [skip ci]

--- test.sh
+++ test.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-set -e
 
 if command -v patdiff
 then
@@ -8,10 +7,24 @@ else
     DIFFTOOL="diff"
 fi
 
-for i in test/*.lox
+RET=0
+
+for i in $(find test -name "*.lox" -type f | sort -R)
 do
     printf "%s" "$i"
     ./lox run "$i" > "${i%.lox}.output" 2>&1
-    $DIFFTOOL "${i%.lox}.expected" "${i%.lox}.output"
-    printf " ✓\n"
+    LOX_RET=$?
+    DIFF=$($DIFFTOOL "${i%.lox}.expected" "${i%.lox}.output")
+    DIFF_RET=$?
+
+    if [ "$LOX_RET" = 0 ] && [ "$DIFF_RET" = 0 ]
+    then
+        printf " ✓\n"
+    else 
+        printf " x\n"
+        RET=1
+        echo "$DIFF"
+    fi
 done
+
+exit $RET
