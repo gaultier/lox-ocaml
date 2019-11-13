a78ec27c3978349fc6f58092822f674b13ddbd08
Philippe Gaultier <philippe.gaultier@ppro.com>
2019-11-13 10:28:03 +0100

    Output all run tests

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-find test -name '*.lox' -type f | parallel  --shuf --timeout 2 './lox run {} > {.}.output 2>&1 && patdiff {.}.expected {.}.output'
+find test -name '*.lox' -type f | parallel  --shuf --timeout 2 './lox run {} > {.}.output 2>&1; patdiff {.}.expected {.}.output && (echo {} "✓" || echo {} "✗")'
