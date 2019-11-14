1dc90e521c6fe439abe5837e10f7cbe705f5b8ac
Philippe Gaultier <philippe.gaultier@ppro.com>
2019-11-14 17:40:40 +0100

    Re-add test for patdiff in test script

--- test.sh
+++ test.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-find test -name '*.lox' -type f | parallel  --shuf --timeout 2 './lox run {} > {.}.output 2>&1; patdiff {.}.expected {.}.output && (echo {} "✓" || echo {} "✗")'
+if command -v patdiff
+then
+    export DIFFTOOL=patdiff
+else
+    export DIFFTOOL="diff"
+fi
+
+find test -name '*.lox' -type f | parallel  --shuf --timeout 2 "./lox run {} > {.}.output 2>&1; $DIFFTOOL {.}.expected {.}.output && (echo {} '✓' || echo {} '✗')"
