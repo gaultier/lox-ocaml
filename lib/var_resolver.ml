99f78846e84e7b7c5e1f9ad35751857dbcf43e78
Philippe Gaultier <philippe.gaultier@ppro.com>
2019-11-15 07:15:41 +0100

    Add declare, define

--- lib/var_resolver.ml
+++ lib/var_resolver.ml
@@ -7,9 +7,20 @@ type scopes = scope Stack.t
 
 let new_scope () : scope = Hashtbl.create (module String)
 
-let var_resolve scopes = function
-  | Block _ ->
+let declare_var scopes name =
+  Stack.top scopes
+  |> Option.iter ~f:(fun scope -> Hashtbl.add_exn scope ~key:name ~data:false)
+
+let define_var scopes name =
+  Stack.top scopes
+  |> Option.iter ~f:(fun scope -> Hashtbl.add_exn scope ~key:name ~data:true)
+
+let rec var_resolve scopes = function
+  | Block stmts ->
       Stack.push scopes (new_scope ()) ;
+      Array.iter ~f:(var_resolve scopes) stmts ;
       Stack.pop_exn scopes |> ignore
+  | Var (Lex.Identifier n, _) ->
+      declare_var scopes n
   | _ ->
       ()
