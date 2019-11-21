cee93d54915a7089cb1dc0d5018633e893141a24
Philippe Gaultier <philippe.gaultier@ppro.com>
2019-11-21 16:45:36 +0100

    --wip-- [skip ci]

--- lib/var_resolver.ml
+++ lib/var_resolver.ml
@@ -5,14 +5,14 @@ type scope = (string, bool) Hashtbl.t
 
 type scopes = scope Stack.t
 
-type resolution = (statement * int) list
+type resolution = (expr * int) list
 
 let new_scope () : scope = Hashtbl.create (module String)
 
 let print_resolution (resolution : resolution) =
   List.iter
     ~f:(fun (k, d) ->
-      let n = match k with Var (Lex.Identifier n, _) -> n | _ -> "unkown" in
+      let n = match k with Variable (Lex.Identifier n) -> n | _ -> "unkown" in
       Stdlib.Printf.printf "- %s: %d\n" n d)
     resolution
 
@@ -24,38 +24,48 @@ let define_var scopes name =
   Stack.top scopes
   |> Option.iter ~f:(fun scope -> Hashtbl.set scope ~key:name ~data:true)
 
-let resolve_local resolution scopes expr n =
+let resolve_local (resolution : resolution) (scopes : scopes) expr n =
   let depth =
     Stack.fold_until ~init:0
       ~f:(fun depth scope ->
         match Hashtbl.find scope n with
         | Some _ -> Stop depth
         | None -> Continue depth)
+      ~finish:(fun depth -> depth)
       scopes
   in
   (expr, depth) :: resolution
 
-let rec var_resolve_scope scopes = function
-  | Block stmts ->
-      Stack.push scopes (new_scope ());
-      Array.iter ~f:(var_resolve_scope scopes) stmts;
-      Stack.pop_exn scopes |> ignore
-  | Var (Lex.Identifier n, _) -> declare_var scopes n
-  | _ -> ()
-
-let resolve stmts =
-  let scopes : scopes = Stack.create () in
-  let resolution : resolution = [] in
-  List.iter ~f:(fun stmt -> var_resolve_scope scopes stmt) stmts;
-  resolution
-
-let var_resolve_expr resolution scopes = function
+let var_resolve_expr (resolution : resolution) (scopes : scopes) = function
   | Variable (Lex.Identifier n) as v ->
+      Stdlib.print_endline "Resolving variable";
       Stack.top scopes
       |> Option.bind ~f:(fun scope -> Hashtbl.find scope n)
       |> Option.iter ~f:(fun b ->
-             if !b then
+             if Bool.equal b false then
                Printf.failwithf
                  "Cannot read local variable `%s` in its own initializer" n ());
       resolve_local resolution scopes v n
   | _ -> resolution
+
+let rec var_resolve_scope (resolution : resolution) (scopes : scopes) = function
+  | Block stmts ->
+      Stack.push scopes (new_scope ());
+      let resolution =
+        Array.fold
+          ~f:(fun resolution stmt -> var_resolve_scope resolution scopes stmt)
+          ~init:resolution stmts
+      in
+      Stack.pop_exn scopes |> ignore;
+      resolution
+  | Var (Lex.Identifier n, _) ->
+      declare_var scopes n;
+      resolution
+  | Print e | Expr e -> var_resolve_expr resolution scopes e
+  | _ -> resolution
+
+let resolve stmts =
+  let scopes : scopes = Stack.create () in
+  List.fold
+    ~f:(fun resolution stmt -> var_resolve_scope resolution scopes stmt)
+    ~init:[] stmts
