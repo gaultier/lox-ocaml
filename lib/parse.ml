fe5ce50794b8c9266c3fe523afdafaa9ddcac2c2
Philippe Gaultier <philippe.gaultier@ppro.com>
2019-11-27 10:13:57 +0100

    add ids everywhere

--- lib/parse.ml
+++ lib/parse.ml
@@ -5,11 +5,18 @@ let ( let* ) x f = Result.bind x ~f
 
 let ( let+ ) x f = Result.map ~f x
 
+type id = int 
+[@@deriving compare, sexp_of]
+
+let id: id ref = ref 0 
+
+let next_id () = id := !id + 1; !id
+
 type function_signature = (value list -> environment -> value[@ignore])
 
 and callable = {
-  arity : int;
-  name : string;
+    arity : (int [@compare.ignore]);
+  name : (string [@compare.ignore]);
   mutable decl_environment : (environment[@ignore]);
   fn : (function_signature[@ignore]);
 }
@@ -23,11 +30,11 @@ and environment = {
 
 and value =
   | Bool of bool
-  | Number of float
+  | Number of float 
   | Nil
-  | String of string
-  | Callable of callable
-[@@deriving compare, sexp_of]
+  | String of string 
+  | Callable of callable 
+[@@deriving sexp_of]
 
 let empty () : env_values_t = Hashtbl.create (module String)
 
@@ -38,40 +45,40 @@ let globals : environment =
         (module String)
         [
           ( "clock",
-            Callable
+            Callable (
               {
                 arity = 0;
                 name = "clock";
                 decl_environment = { values = empty (); enclosing = None };
                 fn = (fun _ _ -> Number (Unix.gettimeofday ()));
-              } );
+              }) );
         ];
     enclosing = None;
   }
 
 type expr =
-  | Binary of expr * token_kind * expr
-  | Grouping of expr
-  | Literal of value
-  | Unary of token_kind * expr
-  | Assign of token_kind * expr
-  | Variable of token_kind
-  | LogicalOr of expr * expr
-  | LogicalAnd of expr * expr
-  | Call of expr * token * expr list
+  | Binary of expr * token_kind * expr * id
+  | Grouping of expr * id
+  | Literal of (value [@compare.ignore]) * id
+  | Unary of token_kind * expr * id
+  | Assign of token_kind * expr * id
+  | Variable of token_kind * id
+  | LogicalOr of expr * expr * id
+  | LogicalAnd of expr * expr * id
+  | Call of expr * token * expr list * id
 [@@deriving compare, sexp_of]
 
 type statement =
-  | Expr of expr
-  | Print of expr
-  | Var of token_kind * expr
-  | Block of statement array
-  | Function of token * token list * statement list
-  | Return of token * expr
-  | IfStmt of expr * statement
-  | IfElseStmt of expr * statement * statement
-  | WhileStmt of expr * statement
-[@@deriving sexp_of]
+  | Expr of expr * id
+  | Print of expr * id
+  | Var of token_kind * expr * id
+  | Block of statement array * id
+  | Function of token * token list * statement list * id
+  | Return of token * expr * id
+  | IfStmt of expr * statement * id
+  | IfElseStmt of expr * statement * statement * id
+  | WhileStmt of expr * statement * id
+[@@deriving compare, sexp_of]
 
 let rec sync acc = function
   (* | For :: _ as r -> *)
@@ -102,17 +109,17 @@ let error ctx expected rest =
       rest )
 
 let rec primary = function
-  | { kind = False; _ } :: rest -> Ok (Literal (Bool false), rest)
-  | { kind = True; _ } :: rest -> Ok (Literal (Bool true), rest)
-  | { kind = Nil; _ } :: rest -> Ok (Literal Nil, rest)
-  | { kind = Number f; _ } :: rest -> Ok (Literal (Number f), rest)
-  | { kind = String s; _ } :: rest -> Ok (Literal (String s), rest)
+  | { kind = False; _ } :: rest -> Ok (Literal (Bool false, next_id ()), rest)
+  | { kind = True; _ } :: rest -> Ok (Literal (Bool true, next_id ()), rest)
+  | { kind = Nil; _ } :: rest -> Ok (Literal (Nil, next_id ()), rest)
+  | { kind = Number f; _ } :: rest -> Ok (Literal (Number f, next_id ()), rest)
+  | { kind = String s; _ } :: rest -> Ok (Literal (String s, next_id ()), rest)
   | { kind = ParenLeft; _ } :: rest -> (
       let* e, rest = expression rest in
       match rest with
-      | { kind = ParenRight; _ } :: rest -> Ok (Grouping e, rest)
+      | { kind = ParenRight; _ } :: rest -> Ok (Grouping (e, next_id ()), rest)
       | _ as rest -> error "Primary" "Expected closing parenthesis `)`" rest )
-  | { kind = Identifier _ as i; _ } :: rest -> Ok (Variable i, rest)
+  | { kind = Identifier _ as i; _ } :: rest -> Ok (Variable (i, next_id ()), rest)
   | _ as rest ->
       error "Primary" "Expected primary (e.g `1` or `(true)` or \"hello\")" rest
 
@@ -134,7 +141,7 @@ and fn_call_comma_arguments args = function
       (fn_call_comma_arguments [@tailcall]) (arg :: args) rest
 
 and fn_call_arguments callee args = function
-  | ({ kind = ParenRight; _ } as t) :: rest -> Ok (Call (callee, t, args), rest)
+  | ({ kind = ParenRight; _ } as t) :: rest -> Ok (Call (callee, t, args, next_id ()), rest)
   | _ as rest ->
       let* expr, rest = expression rest in
       let* args, closing_paren, rest = fn_call_comma_arguments [ expr ] rest in
@@ -143,12 +150,12 @@ and fn_call_arguments callee args = function
         Stdlib.prerr_endline
           (Printf.sprintf
              "Function call: Too many arguments: limit is 255, got: %d" len);
-      Ok (Call (callee, closing_paren, List.rev args), rest)
+      Ok (Call (callee, closing_paren, List.rev args, next_id ()), rest)
 
 and unary = function
   | { kind = Bang as t; _ } :: rest | { kind = Minus as t; _ } :: rest ->
       let+ right, rest = unary rest in
-      (Unary (t, right), rest)
+      (Unary (t, right, next_id ()), rest)
   | _ as t -> (fn_call [@tailcall]) t
 
 and multiplication tokens =
@@ -156,7 +163,7 @@ and multiplication tokens =
   match rest with
   | { kind = Star as t; _ } :: rest | { kind = Slash as t; _ } :: rest ->
       let+ right, rest = multiplication rest in
-      (Binary (left, t, right), rest)
+      (Binary (left, t, right, next_id ()), rest)
   | _ -> Ok (left, rest)
 
 and addition tokens =
@@ -164,7 +171,7 @@ and addition tokens =
   match rest with
   | { kind = Plus as t; _ } :: rest | { kind = Minus as t; _ } :: rest ->
       let+ right, rest = addition rest in
-      (Binary (left, t, right), rest)
+      (Binary (left, t, right, next_id ()), rest)
   | _ -> Ok (left, rest)
 
 and comparison tokens =
@@ -175,7 +182,7 @@ and comparison tokens =
   | { kind = Less as t; _ } :: rest
   | { kind = LessEqual as t; _ } :: rest ->
       let+ right, rest = comparison rest in
-      (Binary (left, t, right), rest)
+      (Binary (left, t, right, next_id ()), rest)
   | _ -> Ok (left, rest)
 
 and equality tokens =
@@ -184,7 +191,7 @@ and equality tokens =
   | { kind = BangEqual as t; _ } :: rest | { kind = EqualEqual as t; _ } :: rest
     ->
       let+ right, rest = equality rest in
-      (Binary (left, t, right), rest)
+      (Binary (left, t, right, next_id ()), rest)
   | _ -> Ok (left, rest)
 
 and expression tokens = (assignment [@tailcall]) tokens
@@ -197,7 +204,7 @@ and assignment = function
       | { kind = Equal; _ } :: rest -> (
           let* a, rest = assignment rest in
           match e with
-          | Variable v -> Ok (Assign (v, a), rest)
+          | Variable (v,_) -> Ok (Assign (v, a, next_id ()), rest)
           | _ -> error "Assignement" "Expected valid assignment target" t )
       | _ -> Ok (e, rest) )
 
@@ -206,7 +213,7 @@ and logic_and tokens =
   match rest with
   | { kind = And; _ } :: rest ->
       let+ r, rest = logic_and rest in
-      (LogicalAnd (l, r), rest)
+      (LogicalAnd (l, r, next_id ()), rest)
   | [] ->
       error "Logical and expression" "Expected and (e.g `true and false`)" rest
   | _ -> Ok (l, rest)
@@ -216,7 +223,7 @@ and logic_or tokens =
   match rest with
   | { kind = Or; _ } :: rest ->
       let+ r, rest = logic_or rest in
-      (LogicalOr (l, r), rest)
+      (LogicalOr (l, r, next_id ()), rest)
   | [] -> error "Logical or expression" "Expected or (e.g `true or false`)" rest
   | _ -> Ok (l, rest)
 
@@ -234,7 +241,7 @@ and expression_stmt = function
 and print_stmt = function
   | { kind = Print; _ } :: rest ->
       let+ expr, rest = expression_stmt rest in
-      (Print expr, rest)
+      (Print (expr, next_id ()), rest)
   | _ as rest ->
       error "Print statement" "Expected print statement (e.g `print 1;`)" rest
 
@@ -247,8 +254,8 @@ and if_stmt = function
           match rest with
           | { kind = Else; _ } :: rest ->
               let+ else_stmt, rest = statement rest in
-              (IfElseStmt (e, then_stmt, else_stmt), rest)
-          | _ -> Ok (IfStmt (e, then_stmt), rest) )
+              (IfElseStmt (e, then_stmt, else_stmt, next_id ()), rest)
+          | _ -> Ok (IfStmt (e, then_stmt, next_id ()), rest) )
       | _ -> error "If statement" "Expected closing `)`" rest )
   | _ as rest ->
       error "If statement" "Expected if statement (e.g `if (true) print 3;`)"
@@ -260,7 +267,7 @@ and while_stmt = function
       match rest with
       | { kind = ParenRight; _ } :: rest ->
           let+ s, rest = statement rest in
-          (WhileStmt (e, s), rest)
+          (WhileStmt (e, s, next_id ()), rest)
       | _ -> error "While statement" "Missing closing `)`" rest )
   | _ as rest ->
       error "While statement" "Expected while statement: (e.g `while(true) {}`)"
@@ -278,14 +285,14 @@ and for_stmt = function
         match rest with
         | { kind = Var; _ } :: _ -> var_decl rest
         | { kind = SemiColon; _ } :: rest ->
-            Ok (Expr (Literal (Bool true)), rest)
+            Ok (Expr (Literal (Bool true , next_id ()), next_id ()), rest)
         | _ ->
             let+ e, rest = expression_stmt rest in
-            (Expr e, rest)
+            (Expr (e, next_id ()), rest)
       in
       let* stop_cond, rest =
         match rest with
-        | { kind = SemiColon; _ } :: rest -> Ok (Literal (Bool true), rest)
+        | { kind = SemiColon; _ } :: rest -> Ok (Literal (Bool true, next_id ()), rest)
         | _ -> (
             let* e, rest = expression rest in
             match rest with
@@ -296,7 +303,7 @@ and for_stmt = function
       in
       let* incr_stmt, rest =
         match rest with
-        | { kind = ParenRight; _ } :: _ -> Ok (Literal (Bool true), rest)
+        | { kind = ParenRight; _ } :: _ -> Ok (Literal (Bool true, next_id ()), rest)
         | _ -> expression rest
       in
       let* _, rest =
@@ -310,10 +317,10 @@ and for_stmt = function
       let* enclosed_body =
         Ok
           (Block
-             [|
+             ([|
                init_clause;
-               WhileStmt (stop_cond, Block [| body; Expr incr_stmt |]);
-             |])
+               WhileStmt (stop_cond, Block ([| body; Expr (incr_stmt , next_id ())|], next_id () ), next_id ());
+             |], next_id ()))
       in
       Ok (enclosed_body, rest)
   | _ as rest -> error "For-loop" "Expected loop (e.g `for (;;)`)" rest
@@ -330,13 +337,13 @@ and block_stmt_inner tokens acc =
 and block_stmt = function
   | { kind = CurlyBraceLeft; _ } :: rest ->
       let+ stmts, rest = block_stmt_inner rest [||] in
-      (Block stmts, rest)
+      (Block (stmts, next_id ()), rest)
   | _ as rest ->
       error "Block statement" "Expected block statement with opening `{`" rest
 
 and return_stmt = function
   | ({ kind = Return; _ } as ret) :: { kind = SemiColon; _ } :: rest ->
-      Ok (Return (ret, Literal Nil), rest)
+      Ok (Return (ret, Literal (Nil, next_id ()), next_id ()), rest)
   | ({ kind = Return; _ } as ret) :: rest ->
       let* e, rest = expression rest in
       let* rest =
@@ -345,7 +352,7 @@ and return_stmt = function
         | _ as rest ->
             error "Return statement" "Expected terminating semicolon `;`" rest
       in
-      Ok (Return (ret, e), rest)
+      Ok (Return (ret, e, next_id ()), rest)
   | _ as rest ->
       error "Return statement" "Expected return statement (e.g `return 1+2;`)"
         rest
@@ -360,18 +367,18 @@ and statement = function
   | { kind = Return; _ } :: _ as t -> return_stmt t
   | _ as t ->
       let+ e, rest = expression_stmt t in
-      (Expr e, rest)
+      (Expr (e, next_id ()), rest)
 
 and var_decl = function
   | { kind = Var; _ }
     :: { kind = Identifier n; _ } :: { kind = Equal; _ } :: rest -> (
       let* e, rest = expression rest in
       match rest with
-      | { kind = SemiColon; _ } :: rest -> Ok (Var (Identifier n, e), rest)
+      | { kind = SemiColon; _ } :: rest -> Ok (Var (Identifier n, e, next_id ()), rest)
       | _ -> error "Variable declaration" "Expected terminating `;`" rest )
   | { kind = Var; _ }
     :: { kind = Identifier n; _ } :: { kind = SemiColon; _ } :: rest ->
-      Ok (Var (Identifier n, Literal Nil), rest)
+      Ok (Var (Identifier n, Literal (Nil, next_id ()), next_id ()), rest)
   | _ as rest ->
       error "Variable declaration"
         "Expected variable declaration (e.g `var x = 1;`)" rest
@@ -383,8 +390,8 @@ and function_decl = function
       let* args, rest = fn_decl_arguments [] rest in
       let* block, rest = block_stmt rest in
       match block with
-      | Block statements ->
-          Ok (Function (name, args, Array.to_list statements), rest)
+      | Block (statements, _) ->
+          Ok (Function (name, args, Array.to_list statements, next_id ()), rest)
       | _ ->
           error "Function declaration"
             "Expected function body (e.g `{ print 1; print 2;}`)" rest )
