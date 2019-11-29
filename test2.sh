d73e279553dcaa77e8bfc128716602c4e982dd11
Philippe Gaultier <philippe.gaultier@ppro.com>
2019-11-29 08:21:56 +0100

    --wip-- [skip ci]

--- test2.sh
+++ test2.sh
@@ -6,4 +6,5 @@ then
 else
     export DIFFTOOL="diff"
 fi
-printf "test/arithmetic.lox\ntest/boolean_logic.lox" | parallel  --shuf --timeout 4 "awk -F '// expect: ' '/expect/{print \$2}' < {} > {.}.expected; ./lox run {} > {.}.output 2>&1; $DIFFTOOL {.}.expected {.}.output && (echo {} '✓' || echo {} '✗')"
+
+find test -name '*.lox' -type f | parallel  --shuf --timeout 4 "awk -F '// expect: ' '/expect/{print \$2}' < {} > {.}.expected; ./lox run {} > {.}.output 2>&1; $DIFFTOOL {.}.expected {.}.output && (echo {} '✓' || echo {} '✗')"
