1017a47cfccf17bcf0631b8a9bcc7a0bafd6b7f8
Philippe Gaultier <philippe.gaultier@ppro.com>
2019-11-29 08:28:21 +0100

    Remove expected files (inlined)

--- test.sh
+++ test.sh
@@ -7,4 +7,4 @@ else
     export DIFFTOOL="diff"
 fi
 
-find test -name '*.lox' -type f | parallel  --shuf --timeout 4 "./lox run {} > {.}.output 2>&1; $DIFFTOOL {.}.expected {.}.output && (echo {} '✓' || echo {} '✗')"
+find test -name '*.lox' -type f | parallel  --shuf --timeout 4 "awk -F '// expect: ' '/expect/{print \$2}' < {} > {.}.expected; ./lox run {} > {.}.output 2>&1; $DIFFTOOL {.}.expected {.}.output && (echo {} '✓' || echo {} '✗')"
