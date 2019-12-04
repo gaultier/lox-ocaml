e80672e68da7650a1e48ca7d149b96c3d3e5daf9
Philippe Gaultier <philippe.gaultier@ppro.com>
2019-12-04 12:15:44 +0100

    Non zero exit code on failed test

--- test.sh
+++ test.sh
@@ -16,4 +16,4 @@ find test -name '*.lox' -type f | sort | parallel -k --timeout 4 " \
     awk -F '// expect: ' '/expect/{print \$2}' < {} > $TMPDIR/{/}.expected; \
     $LOXBIN run {} > $TMPDIR/{/}.output 2>&1; \
     $DIFFTOOL $TMPDIR/{/}.expected $TMPDIR/{/}.output && \
-    printf '\033[32m✓ {}\033[0m\n'  || printf '\033[31m✗ {}\033[0m\n'"
+    printf '\033[32m✓ {}\033[0m\n'  || (printf '\033[31m✗ {}\033[0m\n'; exit 1)"
